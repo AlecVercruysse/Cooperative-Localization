607819b73d52a802e81c80bbc24847ce1f50cea7
Alec Vercruysse <vercruysse.alec@gmail.com>
2022-11-21 12:33:32 -0800

    broken EKF, work on debugging facilities in __main__ of ekf_tools.py

--- src/ekf_tools.py
+++ src/ekf_tools.py
@@ -1,42 +1,137 @@
 import numpy as np
+from tqdm import tqdm
+
+import pdb
+import code
+
+
+def angle_wrap(angle):
+    return (angle + np.pi) % (2 * np.pi) - np.pi
 
 
 class EKFSLAM:
     # keep a whole separate state estimation class
     # so that this can be easily replaced
-    def __init__(self, tot_time):
+    def __init__(self, robot):
         """
         initialize, keep track of state and covariance for all time steps
         """
-        self.tot_time = tot_time  # in units of time steps
+        self.robot = robot
         self.t = 0
         self.state_labels = ["x", "y", "theta"] + \
-            [f"{meas}_{num+1}" for num in range(20) for meas in ["x", "y"]]
+            [f"{meas}_{num+1}" for num in range(5, 20) for meas in ["x", "y"]]
 
-        self.state_hist = np.zeros((tot_time,
+        self.state_hist = np.zeros((self.robot.tot_time,
                                     len(self.state_labels)))
-        self.cov_hist = np.zeros((tot_time,
+        self.cov_hist = np.zeros((self.robot.tot_time,
                                   len(self.state_labels),
                                   len(self.state_labels)))
 
-        # TODO
-        # I feel like a lot of these functions should be implemented
-        # in the robot class, then passed as arguments to the constructor?
-        # to the EKF class? not sure.
-        # propagation step:
-        #   self.calc_prop_Gx(old_state, t)
-        #   self.calc_prop_Gu(old_state, t)
-        #   self.self.get_odom(t)
-        # correction step:
-        #   self.calc_meas_jacobian(est_state)
-        #   self.calc_kalman_gain(est_cov, Ht)
-        #   self.get_meas(t)
-        #   self.calc_meas_prediction(est_state)
-        
-    def iterate(self):
+        # initialize with very large std. devs:
+        # (all initial estimates are random so that the measurement
+        # model does not return a Ht jacobian that divides by zero).
+        # self.state_hist[0] = np.random.random(size=self.state_hist[0].shape)
+        print("warning: using gt for initialization")
+        # pdb.set_trace()
+        gt_x, gt_y, gt_theta = self.robot.get_gt(0)
+        self.state_hist[0] = [gt_x, gt_y, gt_theta] + \
+            [x for num in range(5, 20)
+             for x in np.array(self.robot.get_landmark_gt(num+1))]
+        self.cov_hist[0] = np.ones_like(self.cov_hist[0]) * 1000
+
+    def calc_prop_Gx(self, old_state, t):
+        (v, _), _ = self.robot.get_odom(t)  # need v
+        old_theta = old_state[2]
+
+        # dx, dy, dtheta in terms of dx, dy, dtheta.
+        # see paper, eq (9)
+        robot_Gx = np.array([
+            [1, 0, -v*np.sin(old_theta)*self.robot.dt],
+            [0, 1,  v*np.sin(old_theta)*self.robot.dt],
+            [0, 0, 1]
+        ])
+
+        # mapping does not change in prediction.
+        Gx = np.eye(len(self.state_labels))
+        Gx[0:3, 0:3] = robot_Gx
+        return Gx
+
+    def calc_prop_Gu(self, old_state, t):
+        old_theta = old_state[2]
+
+        Gu = np.zeros((len(self.state_labels), 2))
+        # see paper, eq (10)
+        robot_Gu = np.array([
+            [np.cos(old_theta)*self.robot.dt, 0],
+            [np.sin(old_theta)*self.robot.dt, 0],
+            [0, self.robot.dt]
+        ])
+
+        # mapping does not change in prediction.
+        Gu[0:3, :] = robot_Gu
+        return Gu
+
+    def calc_meas_jacobian(self, est_state, landmark_idx):
+        """
+        For a measurement to a single landmark...
+        the meas model converts robot and landmark location
+        into a range, bearing measurement.
+        Ht linearizes that model, found in eq. (11).
+
+        TODO document parameters
+        """
+        x, y, theta, = est_state[[0, 1, 2]]
+        lx_idx = self.state_labels.index(f"x_{landmark_idx}")
+        ly_idx = self.state_labels.index(f"y_{landmark_idx}")
+        lx, ly = est_state[lx_idx], est_state[ly_idx]
+
+        Ht = np.zeros((2, len(self.state_labels)))
+
+        # add some eps=1e-9 to guard against divide by zero.
+        r = np.sqrt((lx - x)**2 + (ly - y)**2) + 1e-9
+
+        drdx = (x - lx) / r
+        drdy = (y - ly) / r
+        drdt = 0
+
+        dbdx = (ly - y) / r**2
+        dbdy = (x - lx) / r**2
+        dbdt = -1
+
+        Ht_robot = np.array([
+            [drdx, drdy, drdt],
+            [dbdx, dbdy, dbdt]
+        ])
+
+        drdlx = (lx - x) / r
+        drdly = (ly - y) / r
+
+        dbdlx = (y - ly) / r**2
+        dbdly = (lx - x) / r**2
+
+        Ht_landmark = np.array([
+            [drdlx, drdly],
+            [dbdlx, dbdly]
+        ])
+        Ht[:, 0:3] = Ht_robot
+        Ht[:, lx_idx:ly_idx+1] = Ht_landmark
+        return Ht
+
+    def calc_meas_prediction(self, est_state, landmark_idx):
+        x, y, theta, = est_state[[0, 1, 2]]
+        lx_idx = self.state_labels.index(f"x_{landmark_idx}")
+        ly_idx = self.state_labels.index(f"y_{landmark_idx}")
+        lx, ly = est_state[lx_idx], est_state[ly_idx]
+        r = np.sqrt((lx - x)**2 + (ly - y)**2)
+        b = np.arctan2(ly - y, lx - x) - theta
+        return np.array([r, b])
+
+    def iterate(self, debug=False):
         """
         move forward a single time step
         (perform a prediction and correction step).
+
+        TODO print things if debug flag is set.
         """
         old_state, old_cov = self.state_hist[self.t], self.cov_hist[self.t]
         self.t += 1
@@ -47,39 +142,139 @@ class EKFSLAM:
         self.cov_hist[self.t] = new_cov
 
     def predict(self, old_state, old_cov, t):
-        # call a function in robot class to propagate state?
-        # the motion model and the data itself belongs to the robot.
+        # interestingly, the paper uses the nonlinear propagation
+        # function for the state, and only uses the linearization
+        # to propagate the uncertainty. TODO discuss with Prof. Shia
         Gx = self.calc_prop_Gx(old_state, t)  # the jacobian w.r.t. state
         Gu = self.calc_prop_Gu(old_state, t)  # the jacobian w.r.t. odom
-        odometry, odometry_cov = self.get_odom(t)
+        odometry, odometry_cov = self.robot.get_odom(t)
 
         state_est = Gx @ old_state + Gu @ odometry
         cov_est = (Gx @ old_cov @ Gx.T) + (Gu @ odometry_cov @ Gu.T)
+
+        state_est[2] = angle_wrap(state_est[2])
         return state_est, cov_est
 
     def correct(self, est_state, est_cov, t):
         # call a function in robot clas to get measurements?
         # the data belongs to the robot.
-        Ht = self.calc_meas_jacobian(est_state)
-        Kt = self.calc_kalman_gain(est_cov, Ht)
-        meas = self.get_meas(t)
-        meas_prediction = self.calc_meas_prediction(est_state)
+        meas, meas_cov = self.robot.get_meas(t)
+        for landmark in meas:
+            # run the correction step as many times as there are measurements
+            lidx, r, b = landmark
+            if lidx <= 5:
+                continue  # we're not using robot measurements
+            measurement = np.array([r, b])
+            Ht = self.calc_meas_jacobian(est_state, lidx)
+            meas_prediction = self.calc_meas_prediction(est_state, lidx)
 
-        new_state = est_state + Kt @ (meas - meas_prediction)
-        new_cov = (np.identity(len(est_state)) - Kt @ Ht) @ est_cov
+            Kt = est_cov @ Ht.T @ np.linalg.inv(Ht @ est_cov @ Ht.T + meas_cov)
+            est_state = est_state + Kt @ (measurement - meas_prediction)
+            est_cov = (np.identity(len(est_state)) - Kt @ Ht) @ est_cov
+            est_state[2] = angle_wrap(est_state[2])
 
-        return new_state, new_cov
+        return est_state, est_cov
+
+    def get_est_pos(self, t):
+        return self.state_hist[t, 0:3], self.cov_hist[t, 0:3, 0:3]
 
 
 class Robot:
 
-    def __init__(self, df):
-        self.df = 0
-        self.tstep = 0  # keep track of time index
+    def __init__(self, df, fs, landmark_gt=None):
+        self.df = df
+        self.tot_time = len(df)
+        self.dt = 1/fs
+        self.t = 0  # keep track of time index
+        self.landmark_gt = landmark_gt
         self.state_estimator = EKFSLAM(
-            tot_time=len(self.df)
+            robot=self
         )
 
-    def next(self, ):
-        self.tstep += 1
-        self.state_estimator.iterate()
+    def get_odom(self, t):
+        """
+        get odometry info at a timestep.
+
+        Parameters:
+        ----------
+        t : int
+           time index
+
+        Returns:
+        -------
+        odom : tuple of 2 floats
+           v (forward speed in m/s) and omega (angular speed in rad/s)
+        odom_cov : 2x2 np.array
+           covariance matrix of odometry estimates.
+        """
+        odom = self.df.iloc[t][["v", "w"]]
+        odom_cov = np.eye(len(odom)) * 0.05  # TODO odometry covariance!
+        return odom, odom_cov
+
+    def get_meas(self, t):
+        """
+        Get all measurements at a timestep.
+        Returns measurements of other robots as well.
+
+        Parameters:
+        ----------
+        t : int
+           time index
+
+        Returns:
+        -------
+        meas : list of 3-tuples
+           list of tuples containing (subject #, range, bearing)
+           for all measurements at that time stamp. Might be of length zero.
+        meas_cov : 2x2 array
+           covariance matrix of (range, bearing) uncertainty.
+        """
+        s = self.df.iloc[t]
+        meas = [(i+1, s[f"r_{i+1}"], s[f"b_{i+1}"])
+                for i in range(20)
+                if not np.isnan(s[f"r_{i+1}"])]
+        meas_cov = np.eye(2) * 0.05  # TODO!!!!!!!
+        return meas, meas_cov
+
+    def get_gt(self, t):
+        """
+        Get ground-truth position information.
+        """
+        return self.df.iloc[t][["gt_x", "gt_y", "gt_theta"]]
+
+    def get_landmark_gt(self, idx):
+        if self.landmark_gt is None:
+            return None
+        else:
+            df_idx = self.landmark_gt.index[
+                self.landmark_gt["Subject #"] == idx]
+            return self.landmark_gt.iloc[df_idx][["x [m]", "y [m]"]].iloc[0]
+
+    def get_est_pos(self, t):
+        """
+        Get estimated position information.
+        """
+        return self.state_estimator.get_est_pos(t)
+
+    def next(self, callback=lambda x: x, debug=False):
+        self.t += 1
+        self.state_estimator.iterate(debug=debug)
+        callback(self.t)
+
+
+if __name__ == "__main__":
+    import file_tools
+    import visualize
+    import matplotlib.pyplot as plt
+    dfs, landmark_gt = file_tools.get_dataset(1)
+    r = Robot(dfs[0], fs=50, landmark_gt=landmark_gt)
+    scene = visualize.SceneAnimation([r], landmark_gt, title="EKF SLAM",
+                                     plot_est_pos=True, debug=True)
+    plt.ion()
+    plt.show()
+    print("At each time step, press <ENTER> to move to the next," +
+          " or <i> then <ENTER> to start an interactive terminal")
+    for t in tqdm(range(r.tot_time-1)):
+        r.next(debug=True, callback=scene.update_plot)
+        if input() == "i":
+            code.interact(local=locals())
--- src/visualize.py
+++ src/visualize.py
@@ -1,14 +1,36 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 
 import numpy as np
-import pandas as pd
+# import pandas as pd
 # from tqdm import tqdm
+import threading
 
-# import pdb
+import pdb
 import code
 
 colors = ["red", "orange", "lime", "cyan", "orchid"]
+
+
+def get_cov_ellipse_params(x, y, cov):
+    w, v = np.linalg.eig(cov)
+    lambda1, lambda2 = w
+    v1, v2 = v.T
+    alpha = np.arctan2(v1[1], v1[0])
+    major = 2*np.sqrt(5.991*lambda1)
+    minor = 2*np.sqrt(5.991*lambda2)
+    return alpha, major, minor
+
+
+def get_cov_ellipse(x, y, cov, color, alpha=1):
+    """
+    Returns an ellipse patch for a covariance matrix of x,y uncertainty.
+    """
+    alpha, major, minor = get_cov_ellipse_params(x, y, cov)
+    el = mpl.patches.Ellipse((x, y), major, minor, np.rad2deg(alpha),
+                             color=(color), alpha=alpha)
+    return el
 
 
 class RobotVisual:
@@ -19,7 +41,8 @@ class RobotVisual:
     the robot's ID.
     """
 
-    def __init__(self, ax, x, y, theta, name="", color="red"):
+    def __init__(self, ax, robot, name="", color="red",
+                 plot_est_pos=False, plot_est_landmarks=False):
         """
         Create a robot on an axis.
 
@@ -43,22 +66,39 @@ class RobotVisual:
         None
         """
         self.ax = ax
-        self.x = x
-        self.y = y
-        self.theta = theta
+        self.robot = robot
+        self.plot_est_pos = plot_est_pos
+        self.plot_est_landmarks = plot_est_landmarks
+        self.x, self.y, self.theta = self.robot.get_gt(0)
         self.name = name
         self.wheelbase = .235  # m in diameter
 
-        self.circle = plt.Circle((x, y),
+        self.circle = plt.Circle((self.x, self.y),
                                  radius=self.wheelbase/2,
                                  facecolor=color,
                                  edgecolor="black")
-        self.line = plt.Line2D([x, x+self.wheelbase/2*np.cos(self.theta)],
-                               [y, y+self.wheelbase/2*np.sin(self.theta)],
+        self.line = plt.Line2D([self.x,
+                                self.x+self.wheelbase/2*np.cos(self.theta)],
+                               [self.y,
+                                self.y+self.wheelbase/2*np.sin(self.theta)],
                                color="black")
         self.label = self.ax.annotate(name,
-                                      (x+self.wheelbase/2, y+self.wheelbase/2),
+                                      (self.x+self.wheelbase/2,
+                                       self.y+self.wheelbase/2),
                                       color="black")
+
+        if self.plot_est_pos:
+            (x, y, theta), cov = self.robot.get_est_pos(0)
+            cov = cov[:2, :2]  # just x, y
+            self.est_pos = get_cov_ellipse(x, y, cov, color=color, alpha=0.3)
+            self.est_pose = plt.Line2D([x,
+                                        x+self.wheelbase*np.cos(theta)],
+                                       [y,
+                                        y+self.wheelbase*np.sin(self.theta)],
+                                       color="black", alpha=0.3)
+
+        if self.plot_est_landmarks:
+            pass
 
         self.draw()
 
@@ -76,31 +116,41 @@ class RobotVisual:
         """
         self.ax.add_patch(self.circle)
         self.ax.add_line(self.line)
+        if self.plot_est_pos:
+            self.ax.add_patch(self.est_pos)
+            self.ax.add_line(self.est_pose)
 
-    def update(self, x, y, theta):
+    def update(self, frame):
         """
         Update the position of the robot.
 
         Parameters:
         ----------
-        x : float
-           x location of center of robot
-        y : float
-           y location of center of robot
-        theta : float
-           angle of orientation of robot (in radians)
+        frame : int
+           integer time step to plot
 
         Returns:
         -------
         None
         """
-        self.x = x
-        self.y = y
-        self.theta = theta
+        x, y, theta = self.robot.get_gt(frame)
+        self.x, self.y, self.theta = x, y, theta
+
         self.circle.set_center((self.x, self.y))
         self.line.set_data([x, x+self.wheelbase/2*np.cos(self.theta)],
                            [y, y+self.wheelbase/2*np.sin(self.theta)])
         self.label.set_position((x+self.wheelbase/2, y+self.wheelbase/2))
+
+        if self.plot_est_pos:
+            (x, y, theta), cov = self.robot.get_est_pos(frame)
+            cov = cov[:2, :2]  # just x, y
+            alpha, major, minor = get_cov_ellipse_params(x, y, cov)
+            self.est_pos.set_center((x, y))
+            self.est_pos.set_angle(alpha)
+            self.est_pos.set_width(major)
+            self.est_pos.set_height(minor)
+            self.est_pose.set_data([x, x+self.wheelbase/2*np.cos(theta)],
+                                   [y, y+self.wheelbase/2*np.sin(theta)])
 
 
 class SceneAnimation:
@@ -114,20 +164,18 @@ class SceneAnimation:
     to save the animation to a file.
     """
 
-    def __init__(self, dfs, landmark_gt, title="",
+    def __init__(self, robots, landmark_gt, title="",
                  speedup=20, fs=50, undersample=100,
-                 figsize=(5, 8),
+                 plot_est_pos=False, plot_est_landmarks=False,
+                 figsize=(5, 8), debug=False,
                  keys=["gt_x", "gt_y", "gt_theta"]):
         """
         Construct the animation.
 
         Parameters:
         ----------
-        dfs: list of pandas.DataFrame
-           A dataframe for each robot to plot, containing x, y, and orientation
-           data. A crucial assumption is that the length (time) of each
-           dataframe is constant. This function is written to work with the
-           output of file_tools.get_dataset() by default.
+        dfs: list of ekf_tools.Robot objects
+           An object for each robot to plot.
         landmark_gt: pandas.DataFrame
            A dataframe containing landmark ground truth information, generated
            by file_tools.get_dataset().
@@ -154,39 +202,73 @@ class SceneAnimation:
         -------
         self: SceneAnimation object
         """
-        self.dfs = dfs
+        self.robots = robots
+        self.dfs = [r.df for r in self.robots]
         self.landmark_gt = landmark_gt
         self.keys = keys
         self.title = title
         self.fs = fs
         self.length = len(self.dfs[0])  # in frames, of the actual dataset
         self.figsize = figsize
+        self.plot_est_pos = plot_est_pos
+        self.plot_est_landmarks = plot_est_landmarks
 
-        self.xb, self.yb = get_lims(dfs, landmark_gt)
+        self.xb, self.yb = get_lims(self.dfs, landmark_gt)
         self.fig, self.ax = plt.subplots(figsize=self.figsize)
         self.interval = int(1000*undersample/fs/speedup)
-
         pause_time = 3  # seconds
         pause_frames = int(pause_time / (1000*undersample/fs/speedup))
-        self.ani = FuncAnimation(fig=self.fig,
-                                 frames=range(0,
-                                              self.length + pause_frames,
-                                              undersample),
-                                 func=self.update_plot,
-                                 init_func=self.start_plot,
-                                 interval=self.interval,
-                                 blit=False, repeat=False)
+        self.frames = range(0, self.length + pause_frames, undersample)
+
+        if debug:
+            # don't do an animation. step through frame by frame.
+            self.start_plot()
+        #     self.event_loop = threading.Thread(target=self.debug_event_loop)
+        #     self.event_loop.start()
+        else:
+            self.ani = FuncAnimation(fig=self.fig,
+                                     frames=self.frames,
+                                     func=self.update_plot,
+                                     init_func=self.start_plot,
+                                     interval=self.interval,
+                                     blit=False, repeat=False)
+
+    def debug_event_loop(self):
+        for frame in self.frames:
+            print("frame")
+            if frame >= self.length:
+                # this must be an ending pause frame
+                frame = self.length - 1
+            for robot in self.robots:
+                while robot.t <= frame:
+                    pass
+                print("moving on...")
+            self.update_plot(frame)
 
     def start_plot(self):
         """
         Called by the constructor. Initialze the animation
         (plot the first frame).
         """
-        _, self.robots = plot_one_step([df.iloc[0] for df in self.dfs],
-                                       landmark_gt=self.landmark_gt,
-                                       x_bounds=self.xb,
-                                       y_bounds=self.yb,
-                                       ax=self.ax)
+
+        self.anim_robots = []
+        for i in range(len(self.robots)):
+            r = RobotVisual(self.ax, self.robots[i],
+                            name=f"{i+1}", color=colors[i],
+                            plot_est_pos=self.plot_est_pos,
+                            plot_est_landmarks=self.plot_est_landmarks)
+            self.anim_robots += [r]
+
+        for i in self.landmark_gt.index:
+            x, y, name = self.landmark_gt.iloc[i][
+                ["x [m]", "y [m]", "Subject #"]]
+            plt.scatter(x, y, color="black")
+            plt.annotate(int(name), (x, y))
+
+        self.ax.autoscale_view()
+        self.ax.set_xlim(*self.xb)
+        self.ax.set_ylim(*self.yb)
+        self.ax.axis('equal')
         self.ax.set_title(f"{self.title}\n{self._create_time_ctr(0)}")
 
     def update_plot(self, frame):
@@ -204,10 +286,9 @@ class SceneAnimation:
         if frame >= self.length:
             # this must be an ending pause frame
             frame = self.length - 1
-        series = [df.iloc[frame] for df in self.dfs]
-        for i in range(len(series)):
-            x, y, theta = series[i][self.keys]
-            self.robots[i].update(x, y, theta)
+
+        for i in range(len(self.robots)):
+            self.anim_robots[i].update(frame)
         self.ax.set_title(f"{self.title}\n{self._create_time_ctr(frame)}")
 
     def _create_time_ctr(self, frame):
@@ -235,53 +316,56 @@ class SceneAnimation:
         writer = PillowWriter(fps=1000/self.interval)
         self.ani.save(fname, writer=writer)
 
+    def show(self):
+        plt.show()
 
-def plot_one_step(series, landmark_gt=None,
-                  ax=None, x_bounds=None, y_bounds=None, axiseq=True,
-                  fname=None, keys=["gt_x", "gt_y", "gt_theta"]):
-    """
-    Create a matplotlib plot describing a single time frame.
-    Plots landmark and robot positions using the ground-truth
-    dataframes.
 
-    Parameters:
-    ----------
-    series: list of dataframes or dataframe
-       describing the current time frame.
-    """
-    if not isinstance(series, list):
-        series = [series]
-    if not isinstance(series[0], pd.Series):
-        raise ValueError("series needs to be a Series" +
-                         " or list of Series!")
-    if ax is None:
-        _, ax = plt.subplots()
-
-    robots = []
-    for i in range(len(series)):
-        x, y, theta = series[i][keys]
-        # print(f"{x=} {y=} {theta=}")
-        robots += [RobotVisual(ax, x, y, theta,
-                               name=f"{i+1}", color=colors[i])]
-
-    if landmark_gt is not None:
-        for i in landmark_gt.index:
-            x, y, name = landmark_gt.iloc[i][["x [m]", "y [m]", "Subject #"]]
-            plt.scatter(x, y, color="black")
-            plt.annotate(name, (x, y))
-
-    # figure out x limits:
-    ax.autoscale_view()
-    if x_bounds is not None:
-        ax.set_xlim(*x_bounds)
-    if y_bounds is not None:
-        ax.set_ylim(*y_bounds)
-    if axiseq:
-        ax.axis('equal')
-    if fname is not None:
-        plt.savefig(fname)
-
-    return ax, robots
+#def plot_one_step(series, landmark_gt=None,
+#                  ax=None, x_bounds=None, y_bounds=None, axiseq=True,
+#                  fname=None, keys=["gt_x", "gt_y", "gt_theta"]):
+#    """
+#    Create a matplotlib plot describing a single time frame.
+#    Plots landmark and robot positions using the ground-truth
+#    dataframes.
+#
+#    Parameters:
+#    ----------
+#    series: list of dataframes or dataframe
+#       describing the current time frame.
+#    """
+#    if not isinstance(series, list):
+#        series = [series]
+#    if not isinstance(series[0], pd.Series):
+#        raise ValueError("series needs to be a Series" +
+#                         " or list of Series!")
+#    if ax is None:
+#        _, ax = plt.subplots()
+#
+#    robots = []
+#    for i in range(len(series)):
+#        x, y, theta = series[i][keys]
+#        # print(f"{x=} {y=} {theta=}")
+#        robots += [RobotVisual(ax, x, y, theta,
+#                               name=f"{i+1}", color=colors[i])]
+#
+#    if landmark_gt is not None:
+#        for i in landmark_gt.index:
+#            x, y, name = landmark_gt.iloc[i][["x [m]", "y [m]", "Subject #"]]
+#            plt.scatter(x, y, color="black")
+#            plt.annotate(name, (x, y))
+#
+#    # figure out x limits:
+#    ax.autoscale_view()
+#    if x_bounds is not None:
+#        ax.set_xlim(*x_bounds)
+#    if y_bounds is not None:
+#        ax.set_ylim(*y_bounds)
+#    if axiseq:
+#        ax.axis('equal')
+#    if fname is not None:
+#        plt.savefig(fname)
+#
+#    return ax, robots
 
 
 def get_lims(data, landmark_gt,
@@ -314,10 +398,11 @@ def get_lims(data, landmark_gt,
 
 if __name__ == "__main__":
     import file_tools
-
+    import ekf_tools
     # example usage
     dfs, landmark_gt = file_tools.get_dataset(1)
-    s = SceneAnimation(dfs, landmark_gt, title="dataset 1")
+    robots = [ekf_tools.Robot(df, fs=50) for df in dfs]
+    s = SceneAnimation(robots, landmark_gt, title="dataset 1")
     print("\n\ncreated s, an animation object. try either" +
           "\ns.write(\"out.gif\") or \nplt.show()\n\n")
     code.interact(local=locals())
